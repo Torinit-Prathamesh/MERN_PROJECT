2e7667215a4b7a5a2ef1ac32143bd9c391f72c12
Torinit-Prathamesh <104342622+Torinit-Prathamesh@users.noreply.github.com>
2023-09-21 14:40:06 +0530

    order-api

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require("../utils/errorhandler");
 
 module.exports = (err, req, res, next) => {
+  console.log('err: ', err);
   err.statusCode = err.statusCode ?? 500;
   err.message = err.message ?? "Internal Server Error";
   //Wrong MongoDb Id error
